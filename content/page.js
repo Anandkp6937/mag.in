aaba99377affa5d3ab9fce0aceaa6075c3c317fc
anandkp6937 <anandkp6937@gmail.com>
2025-03-13 17:02:31 +0530

    review

--- content/page.js
+++ content/page.js
@@ -1,14 +1,17 @@
 const body=document.body;
 const navigationComponent=document.querySelector('.navigationComponent')
 const themeBtn=document.querySelector('.theme-btn');
+const footer=document.querySelector('footer');
 let flag=0;
 window.addEventListener('DOMContentLoaded',()=>{
      navigationCreater();
+     footerComponent();
+
 })
 function navigationCreater(){
      navigationComponent.innerHTML='';
      navigationComponent.innerHTML=`<div class="page-navigation">
-     <a href="#">
+     <a href="../index.html">
      <div class="logo">
           <img src="../assets/mag-logo-big.png" alt="logo of magnetonn.in" >
      </div>
@@ -19,7 +22,23 @@ function navigationCreater(){
 </div>`
    
 }
+function footerComponent(){
+     footer.innerHTML='';
+     footer.innerHTML=`<div class="Flogo">Magnetonn.in</div>
+          <div class="fquote">
+               <div>"Let noble thoughts come to us from all directions"</div>
 
+          </div>
+               <div class="connect">
+                    <p>Connect with me </p>
+                    <ul>
+                         <li><a href="http://"><i class="fa-brands fa-instagram"></i></a></li>
+                         <li><a href="http://"><i class="fa-solid fa-envelope"></i></a></li>
+                    </ul>
+               </div>
+               <div class="copyright">© 2024 Magneton.in. All Rights Reserved.</div>
+          </div>`
+}
 function themeChanger(){
      if(!flag){
           body.style.background='#000';
--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 let  catogary=document.querySelectorAll('.section');
 let  marked=document.querySelectorAll('.marked');
+let blogWrap=document.querySelector('.blog-container-wrap');
+let lists=document.querySelectorAll('.section');
+let searchBox=document.getElementById('srch');
+let modifiedData;
+let state='all';
+console.log(lists);
+if(state=='all'){
+searchBox.addEventListener('input',searchBlogPost);
+}
+function searchBlogPost(e){
+     let keyWord=e.target.value;
+     let newSortedPostsData=modifiedData.filter((word)=>word.title.toLowerCase().includes(keyWord.toLowerCase()));
+     createPostCard(newSortedPostsData); 
+
+}
+function sortPostBylatest(arr){
+     modifiedData=arr.sort((a, b) => new Date(b.date) - new Date(a.date));
+ }
+function makeCatogaryWisePost(arr,catogary)
+{    
+     if(catogary!='All article'){
+          const modearr=arr.filter((post=>post.category===catogary));
+          blogWrap.innerHTML=''
+          createPostCard(modearr);
+     }
+     else{
+          createPostCard(modifiedData)
+     }
+
+}
+
+
+lists.forEach((cato,i)=>{
+cato.addEventListener('click',(e)=>{
+     let data=e.target.innerText;
+     makeCatogaryWisePost(modifiedData,data);
+})
+
+})
+
 catogary.forEach((item,i)=>{    
      item.addEventListener('click',()=>{
           catogary.forEach((itm)=>itm.classList.remove('marked'));
@@ -7,3 +47,46 @@ catogary.forEach((item,i)=>{
           
      })
 })
+function spiltDate(dates){
+     const dateString=dates;
+     const date = new Date(dateString);
+     const formattedDate = date.toLocaleDateString('en-GB'); 
+     return formattedDate
+}
+function createPostCard(arr){
+     blogWrap.innerHTML ='';
+   arr.forEach((data)=>{
+     blogWrap.innerHTML +=`<div class="blog">
+               <div class="blog-img">
+                    <img src="thumbnail/blog1.jpg" alt="" srcset="">
+               </div>
+               <div class="blog-heading"><a href="content/${data.url}">${data.title}</a></div>
+               
+               <div class="blog-desc">${data.description}
+               </div>
+
+               <div class="blog-details">
+                    <p class="blogdate">${spiltDate(data.date)}</p>
+                    <p class="blogtopic">${data.category}</p>
+               </div>
+            
+          </div>`
+    
+})
+    
+}
+async function grabData(){
+     try{
+          let postData=await fetch('https://magnetonn-in-backend.vercel.app/');
+          datafromApi=await postData.json();
+          sortPostBylatest(datafromApi)
+          createPostCard(modifiedData);
+    
+          
+     }
+     catch(err){
+          console.log('error occured:',err);
+     }
+     
+}
+grabData()
